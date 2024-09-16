2708f73603f5eca62805fada26c040f8c682f08e
Vasileios Almpanis <vasilisalmpanis@gmail.com>
2024-09-16 16:19:29 +0200

    process-and-memory: Test program
    Recursive print of children pid information
    in a more elegant way with corrent indentation.
    The only thing missing is age. Create timeval
    struct and print prettified
    
    Signed-off-by: Vasileios Almpanis <vasilisalmpanis@gmail.com>

--- main.c
+++ main.c
@@ -13,34 +13,50 @@ struct pid_info {
     const unsigned long stack_pointer; // Stack pointer
     unsigned long age;                // Age in jiffies
     pid_t parent_pid;                 // Parent PID
-    pid_t *children;                  // Array of child PIDs
+    pid_t children[100];                  // Array of child PIDs
     char root_path[PATH_MAX];         // Root path
     char current_path[PATH_MAX];      // Current working directory
 };
 
+struct pid_info temp;
+
+void print_info(struct pid_info *info)
+{
+        printf("Process Information:\n");
+        printf("    Process PID      : %d\n", info->pid);
+        printf("    Parent PID       : %d\n", info->parent_pid);
+        printf("    Process State    : %u\n", info->state);
+        printf("    Stack Pointer    : %lx\n", info->stack_pointer);
+	for (int i = 0; i < 16; i++) {
+		if (info->children[i] > 0)
+        		printf("    Child %d, pid     : %d\n", i, info->children[i]);
+	}
+        printf("    Current Path     : %s\n", info->current_path);
+        printf("    Root Path        : %s\n", info->root_path);
+	for (int i = 0; i < 16; i++) {
+		if (info->children[i] > 0) {
+			int ret = syscall(__NR_SYSCALL, &temp, info->children[i]);
+			if (ret == 0)
+				print_info(&temp);
+		}
+	}
+
+}
+
 int main(int argc, char **argv) {
     struct pid_info rets;
 
-    // Check if the correct number of arguments is passed
     if (argc != 2)
         return 0;
 
-    // Convert the argument to an integer representing the PID
     int pid = atoi(argv[1]);
 
-    // Make the system call
     int ret = syscall(__NR_SYSCALL, &rets, pid);
 
-    // Check if the system call succeeded
     if (ret) {
         printf("Error: %d\n", ret);
     } else {
-        // Print the process details in a pretty format
-        printf("Process Information:\n");
-        printf("    Process PID      : %d\n", rets.pid);
-        printf("    Parent PID       : %d\n", rets.parent_pid);
-        printf("    Process State    : %u\n", rets.state);
-        printf("    Stack Pointer    : %lx\n", rets.stack_pointer);
+	    print_info(&rets);
     }
 
     return 0;
